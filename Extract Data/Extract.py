7f9ec47af7f045f5a4f0caed309aad8eca5af9da
Saksh_Patni <patni.s@husky.neu.edu>
2017-07-08 10:49:20 -0700

    Update Extract.py

--- Extract Data/Extract.py
+++ Extract Data/Extract.py
@@ -1,1 +1,17 @@
+import re
 
+file = open('regex_sum_7282.txt', 'r')
+#file = open('Regex.txt', 'r')
+list_integers = list()
+
+for line in file:
+    line = line.rstrip()
+#print (line)
+    integers_list = re.findall('[0-9]+', line )
+#print (integers_list)
+    str_to_int = [int(i) for i in integers_list]
+#print (str_to_int)
+    list_integers.append(sum(str_to_int))
+#print (list_integers)
+    total = sum(list_integers)
+    print (total)
